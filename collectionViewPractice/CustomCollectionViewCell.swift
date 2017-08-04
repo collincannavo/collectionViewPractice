6a79880b6b039aa320a8f61156740a6f4cb936a0
Bop <collin.cannavo@gmail.com>
2017-08-03 20:01:19 -0600

    added custom cell

---
+++ collectionViewPractice/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  collectionViewPractice
+//
+//  Created by Collin Cannavo on 8/3/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
